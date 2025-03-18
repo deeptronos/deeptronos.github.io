fec798f8bb464e08b9742016ad1adfdb1c891c4c
Catherine <c.n@pdx.edu>
2025-03-18 13:49:23 -0700

    added a bunch page content - now let's style everything

--- res/js/script.js
+++ res/js/script.js
@@ -1,56 +1,48 @@
-// const createDynamicFooter = (foot) => { // TBH I don't think a footer would improve this site
-//   const footer = document.createElement("footer");
-//   footer.id = "footer";
-//   footer.classList.add("container-fluid");
-//   footer.classList.add("rounded-top");
-//   footer.classList.add("p-3");
-//   footer.classList.add("mt-3"); // allows for appropriate margin between footer and >vh page content!
-//   // footer.classList.add("mt-auto");
-//   // footer.classList.add("position-absolute");
-//   footer.classList.add("bottom-0");
-//   footer.textContent = "footer :D";
-//   return footer;
+function getPronouns() {
+  const selected = document.querySelector(
+    "input[type='radio'][name='pronoun']:checked",
+  );
+  return selected.id;
+}
+
+function logFormSubmission(event) {
+  const data = [];
+  // const datObj = {};
+  const dataIDs = ["nameInput", "emailInput", "dateInput"];
+
+  for (let dataType in dataIDs) {
+    const thingie = document.getElementById(dataIDs[dataType]);
+    data.push(thingie.value);
+  }
+
+  const pronoun = getPronouns();
+  data.push(pronoun);
+  console.log("data:", data);
+  const form = document.getElementById("form");
+  const formData = new FormData(form);
+  formData.set("pronoun", pronoun);
+  // console.log(formData);
+  // const formattedData = new FormData(
+  //   data[0],
+  //   data[1],
+  //   data[2],
+  //   data[3],
+  //   data[4],
+  //   data[5],
+  // );
+  console.table(formData);
+}
+
+// window.onload = function () {
+//   // createWelcomeContainer();
+//   // getData("https://swapi.dev/api/starships/").then(createSpaceships);
+//   const form = document.getElementById("form");
+
 // };
 
-// document.body.addEventListener("htmx:afterSettle", function (event) {
-//   if (event.target.id == "mainContent") {
-//     const images = event.target.querySelectorAll("img");
-//     if (images.length == 0) {
-//       const old_footer = document.getElementById("footer");
-//       if (old_footer != null) {
-//         old_footer.remove();
-//       }
-
-//       console.log("no images, placing footer immediately.");
-//       const view_area = document.getElementById("mainPage");
-//       view_area.appendChild(createDynamicFooter());
-//       const footer = document.getElementById("footer");
-//       footer.classList.add("position-absolute");
-//     } else {
-//       let loadedCount = 0;
-//       images.forEach((img) => {
-//         if (img.complete) {
-//           loadedCount++;
-//         } else {
-//           img.addEventListener("load", checkAllLoaded);
-//           img.addEventListener("error", checkAllLoaded); // handle!
-//         }
-//       });
-//       function checkAllLoaded() {
-//         loadedCount++;
-//         if (loadedCount == images.length) {
-//           const old_footer = document.getElementById("footer");
-//           if (old_footer != null) {
-//             old_footer.remove();
-//           }
-
-//           console.log("All images loaded, placing footer.");
-//           const view_area = document.getElementById("mainPage");
-//           view_area.appendChild(createDynamicFooter());
-//           const footer = document.getElementById("footer");
-//           footer.classList.add("position-sticky");
-//         }
-//       }
-//     }
-//   }
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("form");
+  if (form) {
+    form.addEventListener("submit", logFormSubmission); // TODO move this from in JS to HTMX?
+  }
+});
